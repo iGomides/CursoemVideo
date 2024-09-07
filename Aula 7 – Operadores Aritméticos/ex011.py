b4e827edde204fc3d26d0b4e8a62e312bc810aee
Yan Gomides <igomides@protonmail.com>
2024-09-07 16:54:14 -0300

    test

--- Aula 7 – Operadores Aritméticos/ex011.py
+++ Aula 7 – Operadores Aritméticos/ex011.py
@@ -1,15 +1,15 @@
 # Exercício 11 – Pintando Parede
 
-'''
+"""
 Faça um programa que leia a largura e a altura de uma parede
 em metros, calcule a sua área e a quantidade de tinta
 necessária para pintá-la, sabendo que cada litro de tinta
 pinta uma área de 2 metros quadrados.
-'''
+"""
 
 altura = float(input("Informe a altura da parede: "))
 largura = float(input("Informe a largura da parede: "))
 metragem = (altura * largura)
-tinta = (metragem / 2)
+tinta = (altura * largura / 2)
 
 print("A parede possui {:.1f}m². Será necessário {:.1f}l de tinta para pinta-la.".format(metragem, tinta))
